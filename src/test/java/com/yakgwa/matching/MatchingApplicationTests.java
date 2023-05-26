3b43746480ed5d62a9a99d87f6f4ef60e06196d6
Ashmin <dbswoals0512@gmail.com>
2023-05-27 01:33:48 +0900

    [프로젝트] 초기화

---
+++ src/test/java/com/yakgwa/matching/MatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakgwa.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
